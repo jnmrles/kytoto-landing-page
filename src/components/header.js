c798765e796a71ca6bfbf73f5de8a8fea8795f88
Jovanni Morales <jnmrles@gmail.com>
2020-04-13 02:55:04 -0500

    main css and header css created. HomePage coded

--- src/components/header.js
+++ src/components/header.js
@@ -1,15 +1,11 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import "./header.css"
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
+  <header>
+    {/* <div
       style={{
         margin: `0 auto`,
         maxWidth: 960,
@@ -27,6 +23,19 @@ const Header = ({ siteTitle }) => (
           {siteTitle}
         </Link>
       </h1>
+    </div> */}
+    <div className="Header">
+      <div className="HeaderGroup">
+        <Link to="/">
+          <img src={require("../images/logo-designcode.svg")} width="30" />
+        </Link>
+        <Link to="/courses">Courses</Link>
+        <Link to="/downloads">Downloads</Link>
+        <Link to="/workshops">Workshops</Link>
+        <Link to="/buy">
+          <button>Buy</button>
+        </Link>
+      </div>
     </div>
   </header>
 )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,25 +1,24 @@
 import React from "react"
 import { Link } from "gatsby"
 
-import Layout from "../components/layout"
+import Layout from "../layouts/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>
-      Learn to <br />
-      design and code React apps
-    </h1>
-    <p>
-      Complete courses about the best tools and design systems. Prototype and
-      build apps with React and Swift.
-    </p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <div className="Hero">
+      <div className="HeroGroup">
+        <h1>Kyoto</h1>
+        <p>
+          A New Generation of <br />
+          E-Commerce Automation
+        </p>
+
+        <Link to="/page-2/">Go to page 2</Link>
+      </div>
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
