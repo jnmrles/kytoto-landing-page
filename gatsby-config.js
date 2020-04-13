83b59f6af78e682c4aff84b6e2456dd4408437e3
Jovanni Morales <jnmrles@gmail.com>
2020-04-13 00:16:08 -0500

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Kyoto Starter`,
+    description: `My First Gatsby Site!`,
+    author: `@Jovanni`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
