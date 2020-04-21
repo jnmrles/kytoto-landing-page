3cceba198e2926dca9c305861f779df7c867c7d3
Jovanni Morales <jnmrles@gmail.com>
2020-04-20 23:05:37 -0500

    success page edits

--- src/pages/success.js
+++ src/pages/success.js
@@ -26,16 +26,20 @@ const Success = () => (
           <Icon />
         </div>
 
-        <p>A New Era of E-Commerce Automation.</p>
+        <p>Thank You!</p>
+        <strong>
+          Please check your email for further instructions on how to complete
+          your account setup.
+        </strong>
         {/* <Link to="/page-2/">Purchase</Link> */}
-        <div className="Logos">
+        {/* <div className="Logos">
           <img src={require("../images/logo-sketch.png")} width="50" />
           <img src={require("../images/logo-figma.png")} width="50" />
           <img src={require("../images/logo-studio.png")} width="50" />
           <img src={require("../images/logo-framer.png")} width="50" />
           <img src={require("../images/logo-react.png")} width="50" />
           <img src={require("../images/logo-swift.png")} width="50" />
-        </div>
+        </div> */}
       </div>
       <div className="testClass">
         <div className="titleWrapper"></div>
