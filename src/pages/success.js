d69c03dad1d8ca172927433db4c8f123a4f56d04
Jovanni Morales <jnmrles@gmail.com>
2020-04-20 22:55:47 -0500

    animated checkout

--- src/pages/success.js
+++ src/pages/success.js
@@ -3,28 +3,46 @@ import Header from "../components/header"
 import "../layouts/layout.css"
 import SEO from "../components/seo"
 import IndexPage from "./index"
+import styled from "styled-components"
+import Wave from "../components/Wave"
+import Icon from "../components/Icon"
+const WaveBottom = styled.div`
+  position: absolute;
+  width: 100%;
+  left: -1px;
+  bottom: -10px;
+  color: black;
+  z-index: 100;
+`
+
 const Success = () => (
-  <>
+  <div>
     <SEO title="Success" />
     <Header />
 
-    <div class="jumbotron text-center">
-      <h1 class="display-3">Thank You!</h1>
-      <p class="lead">
-        <strong>Please check your email</strong> for further instructions on how
-        to complete your account setup.
-      </p>
-      <hr />
-      <p>
-        Having trouble? <a href={IndexPage}>Contact us</a>
-      </p>
-      <p class="lead">
-        <a class="btn btn-primary btn-sm" href="/" role="button">
-          Continue to homepage
-        </a>
-      </p>
+    <div className="jumbotron">
+      <div className="HeroGroup">
+        <div id="Icon">
+          <Icon />
+        </div>
+
+        <p>A New Era of E-Commerce Automation.</p>
+        {/* <Link to="/page-2/">Purchase</Link> */}
+        <div className="Logos">
+          <img src={require("../images/logo-sketch.png")} width="50" />
+          <img src={require("../images/logo-figma.png")} width="50" />
+          <img src={require("../images/logo-studio.png")} width="50" />
+          <img src={require("../images/logo-framer.png")} width="50" />
+          <img src={require("../images/logo-react.png")} width="50" />
+          <img src={require("../images/logo-swift.png")} width="50" />
+        </div>
+      </div>
+      <div className="testClass">
+        <div className="titleWrapper"></div>
+      </div>
+      <Wave />
     </div>
-  </>
+  </div>
 )
 
 export default Success
