2b37c0bdeeb04e5cf9cd9c58b8675fe35e572f6e
jnmrles <jnmrles@gmail.com>
2020-07-16 17:06:44 -0500

    update pricing

--- src/pages/index.js
+++ src/pages/index.js
@@ -151,13 +151,13 @@ const IndexPage = () => {
                       <input
                         type="hidden"
                         name="hosted_button_id"
-                        value="6BPU9ZQHRAFV2"
+                        value="7GZ4AAKCQPRCE"
                       />
                       <table>
                         <tr>
                           <td>
-                            <input type="hidden" name="on0" value="DISCORD" />
-                            DISCORD
+                            <input type="hidden" name="on0" value="Discord:" />
+                            Discord:
                           </td>
                         </tr>
                         <tr>
@@ -168,7 +168,7 @@ const IndexPage = () => {
                       </table>
                       <input
                         type="image"
-                        src="https://www.paypalobjects.com/en_US/i/btn/btn_buynowCC_LG.gif"
+                        src="https://www.paypalobjects.com/en_US/i/btn/btn_subscribeCC_LG.gif"
                         border="0"
                         name="submit"
                         alt="PayPal - The safer, easier way to pay online!"
