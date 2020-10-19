77c12e1a30bc0ebff0ca864105cbd40d55e5a8d7
JM <jnmrles@gmail.com>
2020-10-19 18:00:06 -0500

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -150,7 +150,7 @@ const IndexPage = () => {
 <input type="hidden" name="cmd" value="_s-xclick"/>
 <input type="hidden" name="hosted_button_id" value="R8DWVTE38UPMC"/>
 <table>
-<tr><td><input type="hidden" name="on0" value="DISCORD"/>DISCORD</td></tr><tr><td><input type="text" name="os0" maxlength="200"/></td></tr>
+<tr><td><input type="hidden" name="on0" value="USERNAME"/>USERNAME</td></tr><tr><td><input type="text" name="os0" maxlength="200"/></td></tr>
 </table>
 <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_subscribeCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!"/>
 <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1"/>
